e5f381368bad2581e019cf07646d2bdbc737d008
doomsday26 <harshnamdeo261999@gmail.com>
2022-11-12 09:11:35 +0530

    expanse tracker working fully with axios

--- script.js
+++ script.js
@@ -4,39 +4,52 @@ let category= document.getElementById('category')
 let expanse= document.getElementById('expanse')
 const userList = document.querySelector('#displayList');
 
-
+const http= "https://crudcrud.com/api/4eb53d2d18d74e289956fa02aa920fdc/expanseTracker"
+let unikey;
 
 myForm.addEventListener('submit',add);
 userList.addEventListener('click',removeItem)
 userList.addEventListener('click',EditItem)
-let count=0;
 
-function add(e){
+window.addEventListener('DOMContentLoaded',async()=>{
+  await axios.get(http).then(res=>{ 
+    displayExpanses(res.data)
+   }).catch(err=>{console.log(err)});  
+
+})
+
+async function add(e){
    
 e.preventDefault();
 if(description.value===''||expanse.value===''){ alert('please enter all fields')}
 else{
    
-  let obj={id:count.toString(), expanse:expanse.value, category:category.value,description: description.value}
-  count++;
-  let k = JSON.stringify(obj);
-  localStorage.setItem(obj.id,k);
+  let obj={"expanse":expanse.value, "category":category.value,"description": description.value}
+await axios.post(http,obj).then(res=> console.log(res.data)).catch(err=>{console.log(err);})
+await axios.get(http).then(res=>{ 
+  displayExpanses(res.data)
+  ; }).catch(err=>{console.log(err)});  
 }
+}
+
+//display expanses
+function displayExpanses(data){
+
 //clear previous list items,
 let ul = document.getElementById('displayList');
 while(ul.firstChild){ul.removeChild(ul.lastChild)}
 
 
 //create new childs.
-for (let i = 0; i < localStorage.length; i++) {
-    let destring= JSON.parse(localStorage.getItem(localStorage.key(i)))
-    // console.log(localStorage.key(i),destring.expanse, destring.category,destring.description);
+for (let i = 0; i < data.length  ; i++) {
+    let destring=data[i];
+  //  console.log(destring._id,destring.expanse, destring.category,destring.description);
     
 
 
 //creating li object
 let li= document.createElement('li');
-li.id=destring.id;
+li.id=destring._id;
 li.appendChild(document.createTextNode(destring.expanse + ': ' ))
 li.appendChild(document.createTextNode(destring.category+ " - "))
 li.appendChild(document.createTextNode(destring.description))
@@ -63,36 +76,69 @@ editbtn.appendChild(document.createTextNode('EDIT'))
 li.appendChild(editbtn)
 
 ul.appendChild(li);
-
-
 }
+
 expanse.value=''
 description.value=''
 
 };
 
 
-function removeItem(e){
-    if(e.target.classList.contains('delete')){
+async function removeItem(e){
+  if(e.target.classList.contains('delete')){
     var li= e.target.parentElement;
-
+  
      let key = li.id;
-     console.log(key);
-     localStorage.removeItem(key);
-     userList.removeChild(li);
+    await axios.delete(http+"/"+key).
+     then( async (res)=>{console.log(res.data);
+     }).catch(err=>{console.log(err);})
     }
-}
+  
+   await axios.get(http).then(res=>{
+  displayExpanses(res.data);
+  } )
+  .catch(err=>console.log(err))
+    }
 
-function EditItem(e){
-    if(e.target.classList.contains('edit')){
-      var li= e.target.parentElement;
-      let key = li.id;
-      let destring= JSON.parse(localStorage.getItem(key))
-category.value=destring.category;
-expanse.value=destring.expanse;
-description.value=destring.description;
 
-      localStorage.removeItem(key);
-      userList.removeChild(li);
-     }
-}
+//update the value,
+
+async function EditItem(e){
+
+  if(e.target.classList.contains('edit')){
+  let li= e.target.parentElement;
+  let key = li.id;
+  console.log(key);
+  await axios.get(http+ "/"+key).then( (res)=>{
+expanse.value= res.data.expanse;
+description.value=res.data.description;
+category.value=res.data.category
+  console.log(res.data._id); 
+  } ).catch(err=>console.log(err))
+  
+  unikey=key;
+  
+  myForm.removeEventListener('submit',add)
+  
+  myForm.addEventListener('submit', updated)
+  
+  }
+  
+  }
+  
+  async function updated(e){
+   e.preventDefault();
+    await axios.put(http+"/"+unikey,{
+      "expanse":expanse.value, "category":category.value,"description": description.value
+    }).then(res=>{console.log(res);}).catch(err=>{console.log(err);})
+  
+    myForm.removeEventListener('submit',updated)
+    myForm.addEventListener('submit', add)
+    unikey='';
+  
+  
+    await axios.get(http).then(res=>{
+      displayExpanses(res.data);console.log(res.data);
+      } )
+      .catch(err=>console.log(err))
+  }
